09ab87f205b1829498ec3581227d388e23f7dc20
BrillantZhao <137148873@qq.com>
2017-10-15 15:39:49 +0800

    添加完toolbar上面的红色提示

--- app/src/main/java/com/brilliant/hxim/MainActivity.java
+++ app/src/main/java/com/brilliant/hxim/MainActivity.java
@@ -1,11 +1,22 @@
 package com.brilliant.hxim;
 
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.brilliant.hximlibrary.widget.BadgeActionProvider;
 
 public class MainActivity extends AppCompatActivity {
+
+    // 联系人点击的监听
+    private static final int DELETE_WHAT = 0X01;
+
+    // Toolbar菜单中的购物车按钮。
+    private BadgeActionProvider mDeleteBadgeActionProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
      */
     private void initToolbar2() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar_2);
+        // 必须添加，否则 onCreateOptionsMenu 不会执行
+        setSupportActionBar(toolbar);
         // 设置 toolbar 背景色
         toolbar.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
         //添加溢出菜单
@@ -50,6 +63,37 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    /**
+     * Toolbar自定义菜单，点击监听。
+     */
+    private BadgeActionProvider.OnClickListener onClickListener = new BadgeActionProvider.OnClickListener() {
+        @Override
+        public void onClick(int what) {
+            Toast.makeText(MainActivity.this, "删除", Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.setting_menu, menu);
+
+        // 联系人
+        MenuItem menuItem = menu.findItem(R.id.item_contact);
+        mDeleteBadgeActionProvider = (BadgeActionProvider) MenuItemCompat.getActionProvider(menuItem);
+        mDeleteBadgeActionProvider.setOnClickListener(DELETE_WHAT, onClickListener);
+
+        return true;
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        //=== 控制展示红色标识
+        if (mDeleteBadgeActionProvider != null) {
+            mDeleteBadgeActionProvider.setIcon(R.mipmap.ic_favorite_more);
+            mDeleteBadgeActionProvider.setBadge(99);
+        }
+    }
 
 
 }
---
+++ hximlibrary/src/main/java/com/brilliant/hximlibrary/Test.java
@@ -0,0 +1,9 @@
+package com.brilliant.hximlibrary;
+
+/**
+ * description:
+ * Date: 2017/10/15 15:06
+ * User: zhaobrillant
+ */
+public class Test {
+}
